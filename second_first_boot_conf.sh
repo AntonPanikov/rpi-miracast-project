d4ad1de65a9b1a5054092a9d9cbe2aa917b01473
Anton Panikov <anton.panikov+github@gmail.com>
2021-04-17 01:54:08 -0400

    Initial version of first boot script

---
+++ second_first_boot_conf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Initial setup, should be executed after first boot
+#
+# Optionally first run `sudo raspi-conf` and change Locale and Timezone
+
+# Chnage default `raspberry` password
+echo "Change default use password:"
+passwd
+
